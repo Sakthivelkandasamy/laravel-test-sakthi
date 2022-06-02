3bfd46cb2ab944216bb3bca1283272fcf0ce97fa
Sakthivel <mkcsakthi@gmail.com>
2022-06-02 15:24:00 +0530

    Unit test completed

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+
+	public function children()
+    {
+        return $this->hasMany(MenuItem::class,'parent_id','id');
+    }
+    public function menuitem()
+    {
+        return $this->belongsTo(MenuItem::class);
+    }
+}
